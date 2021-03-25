60e8c1e3fb9890dc29c0616448adb5613a072431
jackcusick95 <jackcusick95@gmail.com>
2021-03-25 15:41:02 -0400

    added gitignor changed course methods

--- skeleton/app/models/course.rb
+++ skeleton/app/models/course.rb
@@ -19,13 +19,13 @@ class Course < ApplicationRecord
         through: :enrollments,
         source: :users
 
-    has_one :prerequisite,
-        primary_key: :prereq_id,
-        foreign_key: :id,
+    belongs_to :prerequisite,
+        primary_key: :id,
+        foreign_key: :prereq_id,
         class_name: :Course
 
-    has_one :instructor,
-        primary_key: :instructor_id,
-        foreign_key: :id,
+    belongs_to :instructor,
+        primary_key: :id,
+        foreign_key: :instructor_id,
         class_name: :User
 end
